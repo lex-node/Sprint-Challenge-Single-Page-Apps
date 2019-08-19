c26ca027e0ac0a9aded2a1159f7ab87c547e8543
triggering-event <gabriel.j.shapiro@gmail.com>
2019-08-18 22:09:43 -0400

    adds semantic-ui styling for character cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,18 +1,24 @@
 import React from "react";
+import {Card, Image} from 'semantic-ui-react'
+import 'semantic-ui/dist/semantic.min.css';
 
 const CharacterCard = (props) => {
     return (
-        <div>
-            <h2>{props.character.name}</h2>
-            <img src={props.character.image} alt={props.character.name}/>
-            <li>Name: {props.character.name}</li>
-            <li>Status: {props.character.status}</li>
-            <li>Species: {props.character.species}</li>
-            <li>Type: {props.character.type}</li>
-            <li>Gender: {props.character.gender}</li>
-            <li>Origin: {props.character.origin.name}</li>
-            <li>Location: {props.character.location.name}</li>
-        </div>
+        <Card>
+            <Image src={props.character.image} alt={props.character.name}/>
+            <Card.Content>
+                <Card.Header>{props.character.name}</Card.Header>
+                <Card.Description>
+                    <li>Name: {props.character.name}</li>
+                    <li>Status: {props.character.status}</li>
+                    <li>Species: {props.character.species}</li>
+                    <li>Type: {props.character.type}</li>
+                    <li>Gender: {props.character.gender}</li>
+                    <li>Origin: {props.character.origin.name}</li>
+                    <li>Location: {props.character.location.name}</li>
+                </Card.Description>
+            </Card.Content>
+        </Card>
 
     )
 }
