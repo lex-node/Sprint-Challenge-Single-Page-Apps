9440d6e10d0bd45533009090ac13c6818d0bf9d5
triggering-event <gabriel.j.shapiro@gmail.com>
2019-08-18 23:02:10 -0400

    adds unstyled nav page & nav functionality

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import CharacterList from "./components/CharacterList";
 import LocationsList from "./components/LocationsList";
 import EpisodeList from "./components/EpisodeList";
 import WelcomePage from "./components/WelcomePage";
+import Header from "./components/Header";
+import TabNav from "./components/TabNav";
 
 export default function App() {
   return (
     <main>
-        <WelcomePage/>
-        <CharacterList/>
-        <LocationsList/>
-        <EpisodeList/>
+        <Header/>
+        <TabNav/>
+       <Route exact path="/" component={WelcomePage}/>
+       <Route exact path="/characters" component={CharacterList}/>
+       <Route exact path="/locations" component={LocationsList}/>
+       <Route exact path="/episodes" component={EpisodeList}/>
     </main>
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import {Tab} from "semantic-ui-react";
+import {NavLink} from "react-router-dom";
 
 // TODO: Add missing tabs below
-export default function TabNav() {
-
+const TabNav = (props) => {
+    return (
+        <div>
+            <Tab.Pane><NavLink exact to="/">Home Page</NavLink></Tab.Pane>
+            <Tab.Pane><NavLink to="/characters">Characters</NavLink></Tab.Pane>
+            <Tab.Pane><NavLink to="/locations">Locations</NavLink></Tab.Pane>
+            <Tab.Pane><NavLink to="/episodes">Episodes</NavLink></Tab.Pane>
+        </div>
+    )
 };
+
+export default TabNav;
+
+/*Add a Router to this application using [React Router](https://reacttraining.com/react-router/web/guides/quick-start).
+
+> **Tip:** Review the [examples on the React Router site.](https://reacttraining.com/react-router/web/guides/quick-start)
+
+- [X] Start by wrapping your `root` component in the `BrowserRouter` component.
+- [ ] Declare your routes with `<Route>` components (for each component created above.)
+  - [ ] Include `/character`, `/location` and `/episode` routes.
+- [ ] In your `App` component, add a navigation menu, [tab bar](https://react.semantic-ui.com/modules/tab/#types-basic), or similar. (Hint: Make sure to use React Routers `NavLink` to link to your different pages.)
+- [ ] If you have any `<a>` tags, replace them with `<Link>` or `<NavLink>` components.
+
+> 💡**Reminder:** `git commit -am 'MVP Completed'`*/
